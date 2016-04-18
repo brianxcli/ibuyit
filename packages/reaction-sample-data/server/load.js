34babc3c7ed43a9040a233eb294d5b6066fee15e
neteay1210@gmail.com <neteasy1210@gmail.com>
2016-04-18 17:08:14 +1200

    simplify shop sample data

--- packages/reaction-sample-data/server/load.js
+++ packages/reaction-sample-data/server/load.js
@@ -9,7 +9,7 @@ if (ReactionCore && ReactionCore.Hooks) {
     ReactionImport.flush(ReactionCore.Collections.Shops);
     // these will flush/import with the rest of the imports from core init.
     ReactionImport.fixture().process(Assets.getText("private/data/Products.json"), ["title"], ReactionImport.load);
-    ReactionImport.fixture().process(Assets.getText("private/data/Tags.json"), ["name"], ReactionImport.load);
+    //ReactionImport.fixture().process(Assets.getText("private/data/Tags.json"), ["name"], ReactionImport.load);
     ReactionImport.flush();
   });
 }
