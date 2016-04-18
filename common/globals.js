c668c6e8fb4d22756b1e35f895d458a4761cb2e5
xiaochen <neteasy1210@gmail.com>
2016-04-18 23:11:12 +1200

    about page

---
+++ common/globals.js
@@ -0,0 +1,4 @@
+/**
+ * Created by xiaochen on 4/18/16.
+ */
+BlazeLayout = Package['kadira:blaze-layout'].BlazeLayout;
--- common/router.js
+++ common/router.js
@@ -1,6 +1,17 @@
 Meteor.startup(function () {
   if (Meteor.isClient) {
     Tracker.autorun(function () {
+      ReactionRouter.route("/about", {
+        name: "about",
+        action: () => {
+          BlazeLayout.render("coreLayout", {
+            template: "IbuyitAbout",
+            layoutHeader: "layoutHeader",
+            layoutFooter: "layoutFooter"
+          });
+        }
+      });
+
       // initialize client routing
       if (ReactionCore.Subscriptions.Packages.ready() && ReactionCore.Subscriptions.Shops.ready()) {
         ReactionRouter.initPackageRoutes();
