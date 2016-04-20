283d245d09313975d140fa3ae0caade0e50534ca
xiaochen <neteasy1210@gmail.com>
2016-04-20 22:30:17 +1200

    navigation highlight

--- client/templates/components/navbar/navbar.js
+++ client/templates/components/navbar/navbar.js
@@ -2,3 +2,29 @@
  * Created by xiaochen on 4/15/16.
  */
 Template.IbuyitNavBar.replaces("CoreNavigationBar");
+
+Template.CoreNavigationBar.helpers({
+  getFocused(current) {
+    // FlowRouter.current() is not reactive, call
+    // watchPathChange() to watch the changes in path
+    ReactionRouter.watchPathChange();
+    console.log(ReactionRouter.current());
+
+    let path = ReactionRouter.current().route.name;
+    if (path === current) {
+      return "focused";
+    } else {
+      return "";
+    }
+  }
+});
+
+Template.CoreNavigationBar.events({
+  'click #home'() {
+    ReactionRouter.go('/');
+  },
+
+  'click #about'() {
+    ReactionRouter.go('/about');
+  }
+});
